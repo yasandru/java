6044f1d26c6c5a50a139fe25500c72b5e8ab0b6e
CobyBoy <aboutacoby@gmail.com>
2023-11-19 19:48:58 -0300

    Matrices

---
+++ src/com/yas/Matrices.java
@@ -0,0 +1,29 @@
+package com.yas;
+
+public class Matrices {
+    public static void main(String[] args) {
+        //Declaracion filas y columnas
+        int matriz[][] = new int[3][3];
+
+        //Asignacion manual
+        matriz[0][0] = 5;
+        matriz[0][1] = 47;
+        matriz[0][2] = 8;
+
+        //Recorrido
+        //fila
+        for (int f = 0; f < 3; f++) {
+            //columna
+            for (int c = 0; c < 3; c++) {
+                System.out.println("El valor de la posicion f: " + f + "c: " + c);
+                System.out.println("es de: "+ matriz[f][c]);
+
+            }
+
+
+        }
+
+
+    }
+
+}
--- src/com/yas/Repetitivas.java
+++ src/com/yas/Repetitivas.java
@@ -1,5 +1,7 @@
 package com.yas;
 
+import java.util.Scanner;
+
 public class Repetitivas {
     public static void main(String[] args) {
         //EjemploWhile
@@ -9,10 +11,46 @@ public class Repetitivas {
             cont = cont+1; */
 
         //EjemploFor
-        for (int cont = 0;cont <= 10;cont++) {
+        /*for (int cont = 0;cont <= 10;cont++) {
             System.out.println("Estoy dando vueltas con FOR, estoy en la vuelta: "+ cont);
-        }
+        }*/
+
+        // ARRAYS
+        // Declaración
+        /* vector de 4 posiciones
+        figura en indice la cantidad de posiciones -1 o 0 al 3*/
+        int vector[] = new int[4];
+
+        //Asignación (manual)
+        /* En cada posición contiene el numero que se le asigna ej. el num 2 se encuentra en la posicion 0. */
+        /*vector[0] = 2;
+        vector[1] = 35;
+        vector[2] = 48;
+        vector[3] = 346;*/
+
+        //Asignacion por teclado
+        Scanner teclado = new Scanner(System.in);
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Ingrese el valor para el indice: " + i);
+            vector[i] = teclado.nextInt();
+            //Esto ultimo quiere decir que la posicion en el indice de mi vector el usuario debe ingresar por teclado un valor Int o entero.
+
+
 
         }
+
+        //Recorrido del array(arreglo)
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Estoy en el indice:" + i);
+            System.out.println("Tengo guardado un:" + vector[i]);
+            System.out.println("----------------------");
+            //Lenght se utiliza para obtener la longitud del array
+
+
+        }
+
+
+    }
+
 
 }
