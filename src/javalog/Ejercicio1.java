e1acf6f3271365746bbafa1d3e21e859e09e7d4a
CobyBoy <aboutacoby@gmail.com>
2023-12-01 23:06:17 -0300

    ejercicios arrays#

--- src/javalog/Ejercicio1.java
+++ src/javalog/Ejercicio1.java
@@ -1,2 +1,28 @@
-package javalog;public class Ejercicio1 {
+package javalog;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+       /*Crea un array de 10 posiciones de números con valores pedidos por teclado.
+        Muestra por consola el indice y el valor al que corresponde.
+       Haz dos métodos, uno para rellenar valores y otro para mostrar.*/
+        int num[] = new int[2];
+        Scanner tec = new Scanner(System.in);
+        for (int i = 0; i < num.length; i++) {
+            System.out.println("Ingresar el valor para el indice " + i);
+            num[i] = tec.nextInt();
+
+
+
+        }
+        for (int i = 0; i < num.length; i++) {
+            System.out.println("En el indice: "+ i + " esta el valor: " + num[i]);
+        }
+        for (int i = 0; i < num.length; i++) {
+            System.out.print("\t" + num[i]);
+        }
+
+
+    }
 }
--- src/javalog/Ejercicio2.java
+++ src/javalog/Ejercicio2.java
@@ -1,2 +1,25 @@
-package javalog;public class Ejercicio2 {
+package javalog;
+
+import java.util.Scanner;
+/*
+*  Crea un array de números donde le indicamos por teclado el tamaño del array, rellenaremos el array con números aleatorios entre 0 y 9,
+* al final muestra por pantalla el valor de cada posición y la suma de todos los valores.
+* Haz un método para rellenar el array (que tenga como parámetros los números entre los que tenga que generar),
+* para mostrar el contenido y la suma del array y un método privado para generar número aleatorio
+* (lo puedes usar para otros ejercicios).
+* */
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Ingrese el tamaño: ");
+        Scanner num = new Scanner(System.in);
+        int size= num.nextInt();
+        int numArray[] = new int[size];
+
+        for (int i = 0; i < numArray.length; i++) {
+            System.out.println("posicion: [" + i + "] = "+ numArray[i]);
+        }
+    }
+
 }
+
